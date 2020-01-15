2702b18295da483dffee6f23a8ca853cb675b11d
yunlong1951 <40911987+yunlong1951@users.noreply.github.com>
2020-01-15 11:08:44 +0800

    Create BootApplication.java

---
+++ src/java/com/example/boot/BootApplication.java
@@ -0,0 +1,23 @@
+package com.example.boot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@MapperScan("com.example.boot.dao")
+public class BootApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(BootApplication.class);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+        System.out.println("=======application is start=======");
+    }
+
+}
