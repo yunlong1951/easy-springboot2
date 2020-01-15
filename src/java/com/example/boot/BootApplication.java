2a0cf7dafaa3b84f28cc19b41ff9c171233922c9
yunlong1951 <t15098891951@gmail.com>
2020-01-15 11:45:29 +0800

    修改配置文件

--- src/java/com/example/boot/BootApplication.java
+++ src/java/com/example/boot/BootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@MapperScan("com.example.boot.dao")
+@MapperScan("com.example.boot.mapper")
 public class BootApplication extends SpringBootServletInitializer {
 
     @Override
