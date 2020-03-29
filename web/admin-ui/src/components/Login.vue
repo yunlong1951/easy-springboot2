e3a7fe1242124d91bc1126fe45286c013e24d450
yunlong <t15098891951@gmail.com>
2020-03-29 22:10:21 +0800

    登录页面

--- web/admin-ui/src/components/Login.vue
+++ web/admin-ui/src/components/Login.vue
@@ -1,69 +1,254 @@
 <template>
-  <el-container>
-    <el-main>
-      <div class="login-panle">
-        <el-form ref="form" :model="form" label-width="80px">
-          <el-form-item label="活动名称">
-            <el-input ></el-input>
-          </el-form-item>
-        </el-form>
+  <div class="login-container">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
+
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
       </div>
-    </el-main>
-  </el-container>
+
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <font-awesome-icon icon="user" />
+        </span>
+        <el-input
+          ref="username"
+          v-model="loginForm.username"
+          placeholder="用户名"
+          name="username"
+          type="text"
+          tabindex="1"
+          autocomplete="on"
+        />
+      </el-form-item>
+
+      <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
+        <el-form-item prop="password">
+          <span class="svg-container">
+
+            <font-awesome-icon icon="lock" />
+          </span>
+          <el-input
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            placeholder="密码"
+            name="password"
+            tabindex="2"
+            autocomplete="on"
+            @keyup.native="checkCapslock"
+            @blur="capsTooltip = false"
+            @keyup.enter.native="handleLogin"
+          />
+          <span class="show-pwd" @click="showPwd">
+            <font-awesome-icon :icon="passwordType === 'password' ? 'eye' : 'eye-slash'" />
+          </span>
+        </el-form-item>
+      </el-tooltip>
+
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+    </el-form>
+
+    <el-dialog title="Or connect with" :visible.sync="showDialog">
+      Can not be simulated on local, so please combine you own business simulation! ! !
+      <br>
+      <br>
+      <br>
+      <social-sign />
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Login"
+  export default {
+    name: "Login",
+    data() {
+      const validatePassword = (rule, value, callback) => {
+        if (value.length < 6) {
+          callback(new Error('密码不能少于6位'))
+        } else {
+          callback()
+        }
+      }
+      const validateUserName = (rule, value, callback) => {
+        if (value == '') {
+          callback(new Error('请输入用户名'))
+        } else {
+          callback()
+        }
+      }
+      return {
+        loginForm: {
+          username: '',
+          password: ''
+        },
+        loginRules: {
+          username: [{required: true, trigger: 'blur', validator: validateUserName}],
+          password: [{required: true, trigger: 'blur', validator: validatePassword}]
+        },
+        passwordType: 'password',
+        capsTooltip: false,
+        loading: false,
+        showDialog: false,
+        redirect: undefined,
+        otherQuery: {}
+      }
+    },methods: {
+      checkCapslock(e) {
+        const { key } = e
+        this.capsTooltip = key && key.length === 1 && (key >= 'A' && key <= 'Z')
+      },
+      handleLogin() {
+        this.$refs.loginForm.validate(valid => {
+          if (valid) {
+            // this.loading = true
+            // this.$store.dispatch('user/login', this.loginForm)
+            //   .then(() => {
+            //     this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
+            //     this.loading = false
+            //   })
+            //   .catch(() => {
+            //     this.loading = false
+            //   })
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      },
+      showPwd() {
+        if (this.passwordType === 'password') {
+          this.passwordType = ''
+        } else {
+          this.passwordType = 'password'
+        }
+        this.$nextTick(() => {
+          this.$refs.password.focus()
+        })
+      },
     }
+  }
 </script>
 
-<style scoped>
-  .el-form label{
-    color: azure;
-    font-weight: bolder;
-  }
-  .el-form{
-    width: 360px;
-    margin: 60px auto;
+<style lang="scss">
+  /* 修复input 背景不协调 和光标变色 */
+  /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-  }
-  .login-panle{
-    width: 900px;
-    height: 500px;
-    background-color: #2c3e50;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 0;
-    margin-bottom: 0;
-    color: azure;
-  }
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
+  $bg:#283443;
+  $light_gray:#fff;
+  $cursor: #fff;
+
+  @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+    .login-container .el-input input {
+      color: $cursor;
+    }
   }
 
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
+  /* reset element-ui css */
+  .login-container {
+    .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
 
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-    height: 960px;
-    display: flex;
-    align-items: center; /*定义body的元素垂直居中*/
-    justify-content: center; /*定义body的里的元素水平居中*/
-  }
-  body > .el-container {
-    margin-bottom: 40px;
-  }
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
 
+        &:-webkit-autofill {
+          box-shadow: 0 0 0px 1000px $bg inset !important;
+          -webkit-text-fill-color: $cursor !important;
+        }
+      }
+    }
+
+    .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+  }
+</style>
+
+<style lang="scss" scoped>
+  $bg:#2d3a4b;
+  $dark_gray:#889aa4;
+  $light_gray:#eee;
+
+  .login-container {
+    min-height: 100%;
+    width: 100%;
+    background-color: $bg;
+    overflow: hidden;
+
+    .login-form {
+      position: relative;
+      width: 520px;
+      max-width: 100%;
+      padding: 160px 35px 0;
+      margin: 0 auto;
+      overflow: hidden;
+    }
+
+    .tips {
+      font-size: 14px;
+      color: #fff;
+      margin-bottom: 10px;
+
+      span {
+        &:first-of-type {
+          margin-right: 16px;
+        }
+      }
+    }
+
+    .svg-container {
+      padding: 6px 5px 6px 15px;
+      color: $dark_gray;
+      vertical-align: middle;
+      width: 30px;
+      display: inline-block;
+    }
+
+    .title-container {
+      position: relative;
+
+      .title {
+        font-size: 26px;
+        color: $light_gray;
+        margin: 0px auto 40px auto;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+
+    .show-pwd {
+      position: absolute;
+      right: 10px;
+      top: 7px;
+      font-size: 16px;
+      color: $dark_gray;
+      cursor: pointer;
+      user-select: none;
+    }
+
+    .thirdparty-button {
+      position: absolute;
+      right: 0;
+      bottom: 6px;
+    }
+
+    @media only screen and (max-width: 470px) {
+      .thirdparty-button {
+        display: none;
+      }
+    }
+  }
 </style>
--- web/admin-ui/src/main.js
+++ web/admin-ui/src/main.js
@@ -4,11 +4,23 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import router from './router'
+
+
+Vue.use(ElementUI);
+
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI);
+
+
 
 /* eslint-disable no-new */
 new Vue({
